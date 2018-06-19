35f5d03f2cd1f32ea3272e1ba503616b7917d704
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2018-06-19 16:50:08 +1000

    Rename plugin to tox-env-hash, tidy module

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='tox-hash',
+    name='tox-env-hash',
     version='0.0.1',
-    py_modules=['tox_hash'],
-    entry_points={'tox': ['tox-hash = tox_hash']},
+    py_modules=['tox_env_hash'],
+    entry_points={'tox': ['tox-env-hash = tox_env_hash']},
     classifiers=['Framework:: tox'],
 )
--- tox_env_hash.py
+++ tox_env_hash.py
@@ -6,8 +6,7 @@ try:
     from pip.download import PipSession
     from pip.req import parse_requirements
 except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
+    # Support for pip 10 - these have been moved to _internal
     from pip._internal.download import PipSession
     from pip._internal.req.req_file import parse_requirements
 
@@ -35,8 +34,8 @@ def deps_hash(deps):
             dep_str_repr = requirements_hash(dep_name)
         else:
             dep_str_repr = dep_name
-        hash_l.append(_str_to_sha1hex(dep_str_repr))
-    return _str_to_sha1hex(''.join(sorted(hash_l)))
+        hash_l.append(str_to_sha1hex(dep_str_repr))
+    return str_to_sha1hex(''.join(sorted(hash_l)))
 
 
 def requirements_hash(req_file_name):
@@ -46,12 +45,6 @@ def requirements_hash(req_file_name):
 
 
 def parse_pip_requirements(requirement_file_path):
-    """
-    Parses requirements using the pip API.
-
-    :param str requirement_file_path: path of the requirement file to parse.
-    :returns list: list of requirements
-    """
     requirements = parse_requirements(
         requirement_file_path,
         session=PipSession()
@@ -59,10 +52,5 @@ def parse_pip_requirements(requirement_file_path):
     return sorted(str(r.req) for r in requirements if r.req)
 
 
-def _str_to_sha1hex(v):
-    """ Turn string into a SHA1 hex-digest.
-
-    >>> _str_to_sha1hex('abc')
-    'a9993e364706816aba3e25717850c26c9cd0d89d'
-    """
+def str_to_sha1hex(v):
     return hashlib.sha1(v.encode('utf-8')).hexdigest()
