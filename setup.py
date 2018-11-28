1c89385ccb12d9e8511f5455557c82128757a2ee
Aaron Ramshaw <aaronramshaw@gmail.com>
2018-11-28 17:00:41 +1100

    Bump version to 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tox-auto-env',
-    version='0.0.1a',
+    version='0.0.1',
     py_modules=['tox_auto_env'],
     entry_points={'tox': ['tox-auto-env = tox_auto_env']},
     classifiers=['Framework :: tox'],
