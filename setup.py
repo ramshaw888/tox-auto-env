3647cf959b9495ec73c6b4f5669a5beff467a88d
Aaron Ramshaw <aaronramshaw@gmail.com>
2018-11-28 16:51:11 +1100

    Fix classifiers section of setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1a',
     py_modules=['tox_auto_env'],
     entry_points={'tox': ['tox-auto-env = tox_auto_env']},
-    classifiers=['Framework:: tox'],
+    classifiers=['Framework :: tox'],
     description='Keeps your tox virtualenv always up to date with dependencies',
     author='Aaron Ramshaw'
 )
