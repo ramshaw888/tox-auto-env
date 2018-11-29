6d60d289218d66a5fe4c88a16c71b3d019ab5490
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2018-11-29 12:11:02 +1100

    Add github repo url to setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,10 @@ setup(
     py_modules=['tox_auto_env'],
     entry_points={'tox': ['tox-auto-env = tox_auto_env']},
     classifiers=['Framework :: tox'],
-    description='Keeps your tox virtualenv always up to date with dependencies',
+    description=(
+        'Keeps your tox virtualenv always up to date with dependencies'),
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Aaron Ramshaw'
+    author='Aaron Ramshaw',
+    url='https://github.com/ramshaw888/tox-auto-env'
 )
